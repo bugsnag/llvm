1c56e8009dcb6aa2bb8e537f1f1c6bc4457a3a4b
Mike White <mike.white@bugsnag.com>
2022-03-11 13:59:52 +0000

    Initial POC

--- include/llvm/DebugInfo/Symbolize/Symbolize.h
+++ include/llvm/DebugInfo/Symbolize/Symbolize.h
@@ -109,3 +109,34 @@ private:
 } // namespace llvm
 
 #endif
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct DebugInformationData {
+  int64_t address;
+  bool inlined;
+  char* fileName;
+  char* shortFunctionName;
+  char* linkageFunctionName;
+  char* symbolTableFunctionName;
+  int line;
+  int column;
+  int startLine;
+} DebugInformationData;
+
+typedef struct WrappedDebugInformationData {
+  int resultCount;
+  DebugInformationData* results;
+  const char* pstrErr;
+} WrappedDebugInformationData;
+
+WrappedDebugInformationData BugsnagSymbolicationTest(const char* file_path, bool includeInline, int64_t addresses[], int addressCount);
+
+#ifdef __cplusplus
+}
+#endif
